0a9f9e574453ea3ea9ad0bdcea6d9e17925b9675
Szymon Sztuka <szymon.sztuka@yahoo.com>
2016-04-29 01:09:03 +0100

    mitch bitfiled encoding, signed integer from string, pre mapping

--- src/python/texit/mitchcodec.py
+++ src/python/texit/mitchcodec.py
@@ -1,6 +1,20 @@
 from xml.dom import minidom
 import binascii
+from decimal import *
 
+getcontext().prec = 5
+
+mitch_names_to_custom_names_map = {"ITCH Add Order" : {#msgtype="0x41"
+"ITCH Nanosecond":"Nanosecond",
+"ITCH Order ID":"OrId",
+"ITCH Side":"Side",
+"ITCH Quantity":"Quantity",
+"LSE ITCH Instrument ID":"InID",
+"ITCH Reserved 1":"R1",
+"ITCH Reserved 2":"R2",
+"ITCH Price":"Price",
+"ITCH Flags":"Flags"},
+}
 
 def chunkfy(data):
     return " ".join(data[i:i+2] for i in range(0, len(data), 2))
@@ -98,7 +112,7 @@ def decode_byte(data, start, length):
 def encode_byte(data):
     return binascii.hexlify(data.encode()).decode()
 
-def decode_bitfield(data, start):
+def decode_bitfield(data, start, length):
     result = []
     for i in range(7, -1, -1):
         if data[start] & (1 << i) == 0:
@@ -109,11 +123,14 @@ def decode_bitfield(data, start):
 
 
 def encode_bitfield(data):
-    result = 0
-    for i in data:
-        result <<= 1
-        result |= (i & 1)
-    return encode_integer(result, 1)
+    if data.startswith("0x"):
+        return encode_integer(int(data,0),1)
+    else:
+        result = 0
+        for i in data:
+            result <<= 1
+            result |= (i & 1)
+        return encode_integer(result, 1)
 
 #####################################################
 #####################################################
@@ -123,11 +140,28 @@ def parse_alpha_with_length(length):
         return decode_alpha(data, start, length)
     return x
 
+
+def encode_price_from_string(data, length):
+    dotIndex = data.find('.')
+    if dotIndex < 0:
+        data += "00000000"
+    else:
+        exiting = len(data)-dotIndex -1
+        for i in range(0,8-exiting):
+            data += "0"
+        data = data[0:dotIndex] + data[dotIndex+1:]
+    return encode_signed_integer(int(data), length)
+
 def encode_price(length):
     def x(data):
-        return encode_signed_integer(int(data), length)
+        return encode_price_from_string(data, length)
     return x
 
+
+def decode_price(decimals):
+    def x(data, start, length):
+        return Decimal(decode_signed_integer(data, start, length)/decimals)
+    return x
 #def encode_b(data):
 #    rev_byte_val = ord(data).to_bytes(1, byteorder='little')
 #    rev_hex_val_unspaced = binascii.hexlify(bytearray(rev_byte_val)).decode()
@@ -138,7 +172,7 @@ doc = minidom.parse("level2-itchmarketdataspecification-5-1-230710.txt")
 sizes = {"Alpha": 0, "BitField": 1, "Byte": 1, "Date": 8, "Time": 8, "Price": 8, "UInt8": 1, "UInt16": 2, "UInt32": 4,
         "UInt64": 8}
 decoders = {"Alpha": decode_alpha, "BitField": decode_bitfield, "Byte": decode_byte, "Date": decode_date,
-            "Time": decode_time, "Price": decode_signed_integer, "UInt8": decode_integer, "UInt16": decode_integer,
+            "Time": decode_time, "Price": decode_price(100000000), "UInt8": decode_integer, "UInt16": decode_integer,
             "UInt32": decode_integer, "UInt64": decode_integer}
 encoders = {"Alpha": encode_alpha, "BitField": encode_bitfield, "Byte": encode_byte, "Date": encode_date,
             "Time": encode_time,
@@ -148,32 +182,26 @@ encoders = {"Alpha": encode_alpha, "BitField": encode_bitfield, "Byte": encode_b
 msgs = doc.getElementsByTagName("msg")
 message_handlers = dict()
 for msg in msgs:
-        print("%s %s" % (msg.getAttribute("msgtype"), msg.getAttribute("name")))
+        #print("%s %s" % (msg.getAttribute("msgtype"), msg.getAttribute("name")))
         fields = msg.getElementsByTagName("fld")
         field_handlers = []
         for field in fields:
-            print("    %s / %s / %s  %s" % (field.getAttribute("datatype"), field.getAttribute("length"),
-                                            field.getAttribute("decimal_points"), field.getAttribute("name")))
+            #print("    %s / %s / %s  %s" % (field.getAttribute("datatype"), field.getAttribute("length"),
+            #                                field.getAttribute("decimal_points"), field.getAttribute("name")))
             field_size = sizes[field.getAttribute("datatype")]
             if field_size == 0:
                 field_size = int(field.getAttribute("length"))
-            field_handlers.append((field.getAttribute("name").replace(" ", ""),field.getAttribute("datatype"),
+            if msg.getAttribute("name") in mitch_names_to_custom_names_map:
+                name = mitch_names_to_custom_names_map[msg.getAttribute("name")][field.getAttribute("name")]
+            else:
+                name = field.getAttribute("name")
+            field_handlers.append((name,field.getAttribute("datatype"),
                                    field_size,encoders[field.getAttribute("datatype")],
                                    decoders[field.getAttribute("datatype")]))
         #print(hex(int(msg.getAttribute("msgtype"),16)))
         message_handlers[hex(int(msg.getAttribute("msgtype"),16))] = field_handlers
 
 
-#print(size["Alpha"])
-
-
-
-##handler = ("0x77": (field,field,files))
-print("message_handlers")
-print(message_handlers)
-
-raw_msg = "0x50 ITCHNanosecond=UInt32 ITCHExecutedQuantity=UInt32 LSEITCHInstrumentID=UInt32 ITCHReserved1=Byte ITCHReserved2=Byte ITCHPrice=Price ITCHTradeID=UInt64 ITCHSideOfAggressor=Byte"
-raw_msg = "0x50 ITCHNanosecond=123456 ITCHExecutedQuantity=1000 LSEITCHInstrumentID=13131313 ITCHReserved1=1 ITCHReserved2=1 ITCHPrice=380 ITCHTradeID=13131313 ITCHSideOfAggressor=1"
 
 def parse_msg(data):
     fields = dict()
@@ -181,32 +209,38 @@ def parse_msg(data):
     for e in elements[1:]:
         pair = e.split("=")
         fields[pair[0]] = pair[1]
-    return(hex(int(elements[0],16)),fields)
+    return(hex(int(clean_value(elements[0].split("=")[1]),16)),fields)
 
-#print(parse_msg(raw_msg))
+def clean_value(elem):
+    return elem
 
-obj = parse_msg(raw_msg)
-handler = message_handlers[obj[0]]
-#print("handler: " + str(type(obj[0])))
-#print(handler)
-print("\nencode:")
-for msg_handler in handler:
-    val = obj[1][msg_handler[0]] #
-    print(val + " + " + msg_handler[1] + "  -> " + msg_handler[3](val))
+def encode_message(message_handlers, raw_msg):
+    obj = parse_msg(raw_msg)
+    handler = message_handlers[obj[0]]
+    result =""
+    for msg_handler in handler:
+        #print(obj[1])
+        if msg_handler[0] in obj[1]:
+            val = obj[1][msg_handler[0]]
+            val = clean_value(val)
+        else:
+            val = "\00"
+        print(val + " + " + msg_handler[1] + "  -> " + msg_handler[3](val))
+        result += msg_handler[3](val)
+        result += " "
+    return result
+
+def decode_message(message_handlers, hex_string_msg):
+    msg_type = message_handlers["0x"+hex_string_msg[0:2]]
+    byte_msg = bytes.fromhex(hex_string_msg)
+    offset = 1
+    for msg_handler in msg_type:
+        val = msg_handler[4](byte_msg,offset,msg_handler[2])
+        print(chunkfy(binascii.hexlify(byte_msg[offset:offset+msg_handler[2]]).decode()) + " + "+msg_handler[1]+" -> " + str(val))
+        offset += msg_handler[2]
 
 
+#print(encode_message(message_handlers,f_txt_msg))
+#decode_message(message_handlers,f_hex_msg)
 
-hex_string_msg = "50 40 e2 01 00 e8 03 00 00 31 5e c8 00 31 31 7c 01 00 00 00 00 00 00 31 5e c8 00 00 00 00 00 31"
 
-#progressing only parser
-#print(hex_msg[0:2])
-print("\ndecode:")
-#key = decode_byte(      bytes.fromhex(hex_msg[0:2])    ,0)
-msg_type = message_handlers["0x"+hex_string_msg[0:2]]
-byte_msg = bytes.fromhex(hex_string_msg)
-
-offset = 1
-for msg_handler in msg_type:
-    val = msg_handler[4](byte_msg,offset,msg_handler[2])
-    print(chunkfy(binascii.hexlify(byte_msg[offset:offset+msg_handler[2]]).decode()) + " + "+msg_handler[1]+" -> " + str(val))
-    offset += msg_handler[2]
