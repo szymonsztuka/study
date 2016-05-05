f6d18986fc434c2195bd630ad0d152d11098f448
Szymon Sztuka <szymon.sztuka@yahoo.com>
2016-05-06 00:31:13 +0100

    encode empty ascii to 00 * field length,  encode integer to fill the field length and truncate exceeding decimals

--- src/python/texit/mitchcodec.py
+++ src/python/texit/mitchcodec.py
@@ -67,11 +67,15 @@ def decode_alpha(data, start, length):
 
 
 def encode_alpha(data, length):
-    return chunkfy(binascii.hexlify(data.encode()).decode())
+    payload = binascii.hexlify(data.encode()).decode()
+    #if len(payload)<2*length:
+        #print("00" * (length - int(len(payload)/2 )))
+    payload += ("00" * (length - int(len(payload)/2)) )
+    return chunkfy(payload)
 
 
 def decode_quoted_alpha(data, start, length):
-    return "'" + decode_alpha(data, start, length) + "'"
+    return "'" + decode_alpha(data, start, length).strip('\0')+ "'"
 
 
 def encode_quoted_alpha(data, length):
@@ -125,7 +129,8 @@ def encode_bitfield(data, length):
 def decode_annotated_bitfield(decorators):
     def decode_annotated_bitfield_inner(data, start, length):
         bit_array = decode_bitfield(data, start, length)
-        return "0" + str(data[start:start+length])[3:-1] + "/" \
+        return "0x" + binascii.hexlify(bytearray(data[start:start+length])).decode() \
+               + "/" \
                + ",".join([elem[1] + "=" + bit_array[elem[0] - 1] for elem in decorators])
     return decode_annotated_bitfield_inner
 
@@ -139,7 +144,8 @@ def encode_annotated_bitfield(data, length):
 def decode_price(implied_precision):
     def decode_price_inner(data, start, length):
         division = 10 ** implied_precision
-        return "%0.8f" % float(Decimal(decode_signed_integer(data, start, length)/division))  # TODO pretty print 8 is hardcoded
+        format = "%0." + str(implied_precision) + "f"  # TODO cache formats
+        return format % float(Decimal(decode_signed_integer(data, start, length)/division))
     return decode_price_inner
 
 
@@ -150,9 +156,12 @@ def encode_price(implied_precision):
             data += ("0"*implied_precision)
         else:
             exiting = len(data) - dot_index - 1
-            for i in range(0,implied_precision-exiting):
+            for i in range(0, implied_precision - exiting):
                 data += "0"
-            data = data[0: dot_index] + data[dot_index + 1:]
+            if implied_precision - exiting < 0:
+                data = data[0: dot_index] + data[dot_index + 1:implied_precision - exiting]
+            else:
+                data = data[0: dot_index] + data[dot_index + 1:]
         return encode_signed_integer(int(data), length)
     return encode_price_inner
 
@@ -201,45 +210,88 @@ mitch_handlers_1 = {
     hex(int("0x41", 16)): (
         #"length": "length",
         ("type", 1, decode_annotated_hex("AddOrder"), encode_annotated_hex),
-        ("Nanosecond",),
-        ("OrderID",),
-        ("Side",),
-        ("Quantity",),
-        ("InstrumentID",),
-        ("Rsv1",),
-        ("Rsv2",),
-        ("Price",),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
+        ("Side", 1, decode_quoted_alpha, encode_quoted_alpha),
+        ("Quantity", 8, decode_integer, encode_integer),
+        ("InstrumentID", 4, decode_integer, encode_integer),
+        ("Rsv", 2, decode_blank, encode_blank),
+        ("Price", 8, decode_price(8), encode_price(8)),  # 8 decimal points
         ("Flags", 1, decode_annotated_bitfield({(1, "bit1")}), encode_annotated_bitfield)
     ),
-    hex(int("0x46", 16)): (
+    hex(int("0x42", 16)): (
         #"length": "length",
-        ("type", 1, decode_annotated_hex("AddAttributedOrder"), encode_annotated_hex),
-        ("Nanosecond",),
-        ("OrdID",),
-        ("Side",),
-        ("Quantity",),
-        ("InstrumentID",),
-        ("Rsv1",),
-        ("Rsv2",),
-        ("Price",),
-        ("Attribution",),
-        ("Flags", 1, decode_annotated_bitfield({(2, "bit2"), (3, "bit3")}), encode_annotated_bitfield)
+        ("type", 1, decode_annotated_hex("TradeBreak"), encode_annotated_hex),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("TradeID", 8, decode_integer, encode_integer),
+        ("TradeType", 1, decode_quoted_alpha, encode_quoted_alpha),
+        ("InstrumentID", 4, decode_integer, encode_integer),
+    ),
+    hex(int("0x43", 16)): (
+        #"length": "length",
+        ("type", 1, decode_annotated_hex("OrderExecutedWithPrice"), encode_annotated_hex),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
+        ("ExecutedQuantity", 4, decode_integer, encode_integer),
+        ("DisplayQuantity", 4, decode_integer, encode_integer),
+        ("TradeID", 8, decode_integer, encode_integer),
+        ("Printable", 1, decode_quoted_alpha, encode_quoted_alpha),
+        ("Price", 8, decode_price(8), encode_price(8)),  # 8 decimal points
     ),
     hex(int("0x44", 16)): (
         #"length": "length",
         ("type", 1, decode_annotated_hex("OrderDeleted"), encode_annotated_hex),
-        ("Nanosecond",),
-        ("OrderID",),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
         ("Flags", 1, decode_annotated_bitfield({(4, "bit4")}), encode_annotated_bitfield),
-        ("InstID",),
+        ("InstrumentID", 4, decode_integer, encode_integer),
+    ),
+    hex(int("0x45", 16)): (
+        #"length": "length",
+        ("type", 1, decode_annotated_hex("OrderExecuted"), encode_annotated_hex),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
+        ("ExecutedQuantity", 4, decode_integer, encode_integer),
+        ("TradeID", 8, decode_integer, encode_integer)
+    ),
+    hex(int("0x46", 16)): (
+        #"length": "length",
+        ("type", 1, decode_annotated_hex("AddAttributedOrder"), encode_annotated_hex),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
+        ("Side", 1, decode_quoted_alpha, encode_quoted_alpha),
+        ("Quantity", 8, decode_integer, encode_integer),
+        ("InstrumentID", 4, decode_integer, encode_integer),
+        ("Rsv", 2, decode_blank, encode_blank),
+        ("Price", 8, decode_price(8), encode_price(8)),  # 8 decimal points
+        ("Attribution", 11, decode_quoted_alpha, encode_quoted_alpha),
+        ("Flags", 1, decode_annotated_bitfield({(2, "bit2"), (3, "bit3")}), encode_annotated_bitfield)
+    ),
+    hex(int("0x47", 16)): (
+        #"length": "length",
+        ("type", 1, decode_annotated_hex("SymbolStatus"), encode_annotated_hex),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("InstrumentID", 8, decode_integer, encode_integer),
+        ("Rsv", 2, decode_blank, encode_blank),
+        ("TradingStatus", 1, decode_quoted_alpha, encode_quoted_alpha),
+        ("Flags", 1, decode_annotated_bitfield({(5, "bit5")}), encode_annotated_bitfield),
+        ("HaltReason", 4, decode_quoted_alpha, encode_quoted_alpha),
+        ("SessionChangeReason", 1, decode_integer, encode_integer),
+        ("NewEndTime", 8, decode_quoted_alpha, encode_quoted_alpha),
+        ("BookType", 1, decode_integer, encode_integer),
+    ),
+    hex(int("0x54", 16)): (
+        #"length": "length",
+        ("type", 1, decode_annotated_hex("Time"), encode_annotated_hex),
+        ("Seconds", 4, decode_integer, encode_integer),
     ),
     hex(int("0x55", 16)): (
         #"length": "length",
         ("type", 1, decode_annotated_hex("OrderModified"), encode_annotated_hex),
-        ("Nanosecond",),
-        ("OrderID",),
-        ("NewQuantity",),
-        ("NewPrice",),
+        ("Nanosecond", 4, decode_integer, encode_integer),
+        ("OrderID", 8, decode_integer, encode_integer),
+        ("NewQuantity", 4, decode_integer, encode_integer),
+        ("NewPrice", 8, decode_price(8), encode_price(8)),  # 8 decimal points
         ("Flags", 1, decode_annotated_bitfield({(0, "bit0"), (5, "bit5")}), encode_annotated_bitfield)
     ),
     hex(int("0x79", 16)): (
@@ -247,32 +299,8 @@ mitch_handlers_1 = {
         ("type", 1, decode_annotated_hex("OrderBookClear"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("InstrumentID", 4, decode_integer, encode_integer),
-        ("Rsv1", 2, decode_blank, encode_blank),
+        ("Rsv", 2, decode_blank, encode_blank),
         ("Flags", 1, decode_annotated_bitfield({(5, "bit5")}), encode_annotated_bitfield),
-    ),
-    hex(int("0x54", 16)): (
-        #"length": "length",
-        ("type", 1, decode_annotated_hex("Time"), encode_annotated_hex),
-        ("ITCH Seconds", "Secs")
-    ),
-    hex(int("0x45", 16)): (  # "ITCH Order Executed"
-        #"length": "length",
-        ("type", 1, decode_annotated_hex("OrderExecuted"), encode_annotated_hex),
-        ("Nanosecond", 4, decode_integer, encode_integer),
-        ("OrderID", 8, decode_integer, encode_integer),
-        ("ExecutedQuantity", 8, decode_integer, encode_integer),
-        ("TradeID", 8, decode_integer, encode_integer)
-    ),
-    hex(int("0x43", 16)): (
-        #"length": "length",
-        ("type", 1, decode_annotated_hex("OrderExecutedWithPrice"), encode_annotated_hex),
-        ("Nanosecond",),
-        ("OrderID",),
-        ("ExecutedQuantity",),
-        ("DisplayQuantity",),
-        ("TradeID",),
-        ("Printable",),
-        ("Price",)
     )
 }
 
@@ -293,14 +321,16 @@ mitch_handlers_2 = {
     )
 }
 
+#https://www.jse.co.za/services/technologies/equity-market-trading-and-information-technology-change
+mitch_handlers_3 = {
 
+}
 type_from_tag_vals = lambda tag_values: hex(int(tag_values.get("type")[:tag_values.get("type").index("/")], 16))
 type_from_hex_string = lambda value: hex(int("0x" + value[3:5],16))
 
 ######################################
 
-def test_from_text_msg(schema, text_message):
-    print(schema)
+def test_from_text_msg(schema, type_from_tag_vals, text_message):
     msg_tag_values = parse_msg(text_message)
     hex_message = encode_msg_from_tag_vals(schema, type_from_tag_vals, msg_tag_values)
     print(text_message)
@@ -309,6 +339,20 @@ def test_from_text_msg(schema, text_message):
     print("")
 
 
-test_from_text_msg(mitch_handlers_1, "type=0x79/OrderBookClear Nanosecond=34344000 InstrumentID=21472 Flags=0x08/bit5=1")
+#test_from_text_msg(mitch_handlers_1, "type=0x41/AddOrder Nanosecond=007706000 OrderID=363367994316859631 Side='S' Quantity=2500 InstrumentID=37908 Price=42.95000000 Flags=0x00/bit1=0")
 
-test_from_text_msg(mitch_handlers_2, "type=0x41/AddOrder Nanosecond=1 OrderID=2 Side='B' Quantity=34 InstrumentID=4 Price=5.00000000 Flags=0x01/bit1=0")
+#test_from_text_msg(mitch_handlers_1, "type=0x46/AddAttributedOrder Nanosecond=683608000 OrderID=363526361572180373 Side='B' Quantity=200000 InstrumentID=5090 Price=33.00000000 Attribution='WNTSGB2LBIC' Flags=0x20/bit2=0,bit3=1")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x44/OrderDeleted Nanosecond=088393000 OrderID=363526361572180374 Flags=0x00/bit4=0 InstrumentID=5081")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x55/OrderModified Nanosecond=306445000 OrderID=363526361572180587 NewQuantity=200000 NewPrice=36.00000000 Flags=0x08/bit0=0,bit5=1")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x79/OrderBookClear Nanosecond=34344000 InstrumentID=21472 Flags=0x08/bit5=1")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x54/Time Seconds=18010")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x45/OrderExecuted Nanosecond=211867000 OrderID=363526341171088975 ExecutedQuantity=31 TradeID=1420024233328668")
+
+#test_from_text_msg(mitch_handlers_1, "type=0x43/OrderExecutedWithPrice Nanosecond=177651000 OrderID=363526341171092635 ExecutedQuantity=7 DisplayQuantity=2 TradeID=1420024233328641 Printable='N' Price=755.00000000")
+
+#test_from_text_msg(mitch_handlers_2, "type=0x41/AddOrder Nanosecond=1 OrderID=2 Side='B' Quantity=34 InstrumentID=4 Price=5.00000000 Flags=0x01/bit1=0")
