dc75895af84eaa0f2f6a71acb1ef5b1e550b086a
Szymon Sztuka <szymon.sztuka@yahoo.com>
2016-05-09 23:53:38 +0100

    message type handlers instead of field type handlers

--- src/python/texit/mitch.py
+++ src/python/texit/mitch.py
@@ -1,4 +1,5 @@
 import binascii
+import mitchcodec as c
 
 hex_val = "00 10 00 00"
 byte_val = bytes.fromhex(hex_val)
@@ -19,177 +20,85 @@ print("rev_hex_val " + str(type(rev_hex_val)) + " " + str(rev_hex_val) + " len="
 print("rev_hex_val2 " + str(type(rev_hex_val2)) + " " + str(rev_hex_val2) + " len=" + str(len(rev_hex_val2)))
 
 
-def chunkfy(data):
-    return " ".join(data[i:i+2] for i in range(0, len(data), 2))
+#test_from_text_msg(mitch_handlers_1, "type=0x41/AddOrder Nanosecond=007706000 OrderID=363367994316859631 Side='S' Quantity=2500 InstrumentID=37908 Price=42.95000000 Flags=0x00/bit1=0")
 
+#test_from_text_msg(mitch_handlers_1, "type=0x46/AddAttributedOrder Nanosecond=683608000 OrderID=363526361572180373 Side='B' Quantity=200000 InstrumentID=5090 Price=33.00000000 Attribution='WNTSGB2LBIC' Flags=0x20/bit2=0,bit3=1")
 
-def parse_integer(data, start, length):
-    return int.from_bytes(data[start: start + length], byteorder='little')
+#test_from_text_msg(mitch_handlers_1, "type=0x44/OrderDeleted Nanosecond=088393000 OrderID=363526361572180374 Flags=0x00/bit4=0 InstrumentID=5081")
 
+#test_from_text_msg(mitch_handlers_1, "type=0x55/OrderModified Nanosecond=306445000 OrderID=363526361572180587 NewQuantity=200000 NewPrice=36.00000000 Flags=0x08/bit0=0,bit5=1")
 
-def encode_integer(data, length):
-    rev_byte_val = data.to_bytes(length,'little')
-    rev_hex_val_unspaced = binascii.hexlify(bytearray(rev_byte_val)).decode()
-    return chunkfy(rev_hex_val_unspaced)
+#test_from_text_msg(mitch_handlers_1, "type=0x79/OrderBookClear Nanosecond=34344000 InstrumentID=21472 Flags=0x08/bit5=1")
 
+#test_from_text_msg(mitch_handlers_1, "type=0x54/Time Seconds=18010")
 
-def parse_signed_integer(data, start, length):
-    rest_byte = 0x7f & data[0]  # 127
-    res = int.from_bytes(rest_byte.to_bytes(2, 'little') + data[start + 1: start + 1 + length], byteorder='little')
-    if data[0] & 0x80 == 0:  # 128
-        return res
-    else:
-        return - res
+#test_from_text_msg(mitch_handlers_1, "type=0x45/OrderExecuted Nanosecond=211867000 OrderID=363526341171088975 ExecutedQuantity=31 TradeID=1420024233328668")
 
+#test_from_text_msg(mitch_handlers_1, "type=0x43/OrderExecutedWithPrice Nanosecond=177651000 OrderID=363526341171092635 ExecutedQuantity=7 DisplayQuantity=2 TradeID=1420024233328641 Printable='N' Price=755.00000000")
 
-def parse_signed_integer2(data, start, length):
-    last_byte_index = start + length - 1
-    rest_byte = 0x7f & data[last_byte_index]  # 127
-    res = int.from_bytes(data[start: last_byte_index], byteorder='little')
-    result = (rest_byte << 8 * (length - 1)) + res
-    if data[last_byte_index] & 0x80 == 0:  # 128
-        return result
-    else:
-        return - result
+#test_from_text_msg(mitch_handlers_2, "type=0x41/AddOrder Nanosecond=1 OrderID=2 Side='B' Quantity=34 InstrumentID=4 Price=5.00000000 Flags=0x01/bit1=0")
 
+print(c.decode_integer(bytes.fromhex("00 10"),0,2))
+print(c.decode_integer(bytes.fromhex("00 10 00 00"),0,4))
 
-def encode_signed_integer2(data, length):
-    if data < 0:
-       rev_byte_val = (-data).to_bytes(length, 'little')
-       sign_byte = rev_byte_val[length - 1] | 0x80  # 128
-       rev_hex_val_unspaced = binascii.hexlify(bytearray(rev_byte_val[0:length - 1] + sign_byte.to_bytes(1, 'little'))).decode()
-    else:
-       rev_byte_val = data.to_bytes(length, 'little')
-       rev_hex_val_unspaced = binascii.hexlify(bytearray(rev_byte_val)).decode()
-    return chunkfy(rev_hex_val_unspaced)
+print(c.decode_signed_integer(bytes.fromhex("87"),0,1))
+print(c.decode_signed_integer(bytes.fromhex("07"),0,1))
+print(c.decode_signed_integer(bytes.fromhex("07 80"),0,2))
+print(c.decode_signed_integer(bytes.fromhex("07 00"),0,2))
+print(c.decode_signed_integer(bytes.fromhex("07 00 00 80"),0,4))
+print(c.decode_signed_integer(bytes.fromhex("07 00 00 00"),0,4))
+print(c.decode_signed_integer(bytes.fromhex("6c 08 00 80"),0,4))
+print(c.decode_signed_integer(bytes.fromhex("6c 08 00 00"),0,4))
 
+print(c.decode_bitfield(bytes.fromhex("e5"),0,1))
 
-def parse_signed_integer3(data, start, length):
-    res = int.from_bytes(data[start: start + length], byteorder='little')
-    if data[0] & 0x80 == 0:  # 128
-        return res & ((~(1 << 15)) & 0xffff)
-    else:
-        return - (res & (~(1 << 15) & 0xffff))
+print(c.decode_hex(bytes.fromhex("53"),0,1))
+print(c.decode_hex(bytes.fromhex("73"),0,1))
 
+#print(c.parse_date(bytes.fromhex("32 30 31 36 30 34 32 36"),0))
+#print(c.parse_date(bytes.fromhex("31 39 39 39 31 32 33 31"),0))
+#print(c.parse_time(bytes.fromhex("31 33 3a 32 38 3a 30 37"),0))
 
-from datetime import datetime
+#print(c.parse_alpha(bytes.fromhex("61 62 63 64 65 66 67 24 31 33 3f"),0,11))
 
+print(c.encode_integer(4096,2))
+print(c.encode_signed_integer(-7,1))
+print(c.encode_signed_integer(7,1))
+print(c.encode_signed_integer(-7,2))
+print(c.encode_signed_integer(7,2))
+print(c.encode_signed_integer(-7,4))
+print(c.encode_signed_integer(7,4))
+print(c.encode_signed_integer(-2156,4))
+print(c.encode_signed_integer(2156,4))
 
-def parse_time(data, start):
-    """HH:MM:SS"""
-    return datetime.strptime(data[start:start+8].decode(), '%H:%M:%S').time()
+#print(encode_byte("S"))
+#print(encode_byte("s"))
 
+#print(encode_alpha("abcdefg$13?"))
 
-def encode_time(data):
-    """HH:MM:SS"""
-    return encode_alpha(data)
+#print(encode_date("20160426"))
+#print(encode_date("19991231"))
+#print(encode_time("13:28:07"))
 
+#print(parse_signed_integer2(bytes.fromhex("00 e1 f5 05 00 00 00 80"),0,8))
+#print(encode_signed_integer2(-100000000,8))
+#print(parse_signed_integer2(bytes.fromhex("00 e1 f5 05 00 00 00 00"),0,8))
+#print(encode_signed_integer2(100000000,8))
 
-def parse_date(data, start):
-    """YYYYMMDD"""
-    return datetime.strptime(data[start:start+8].decode(), '%Y%m%d')
+#print(parse_signed_integer2(bytes.fromhex("d0 50"),0,2))
+#print(parse_signed_integer2(bytes.fromhex("d0 d0"),0,2))
+#print(encode_signed_integer2(288,2))
 
+#print(parse_signed_integer2(bytes.fromhex("d0"),0,1)) #ok
+#print(parse_signed_integer2(bytes.fromhex("50"),0,1)) #ok
 
-def encode_date(data):
-    """YYYYMMDD"""
-    return encode_alpha(data)
+#print(parse_signed_integer2(bytes.fromhex("00 d0"),0,2))
+#print(parse_signed_integer2(bytes.fromhex("00 50"),0,2)) # -> 20480
 
+#print(encode_signed_integer2(-20480,2))
+#print(encode_signed_integer2(20480,2))
 
-def parse_alpha(data, start, length):
-    return data[start:start+length].decode()
+#x = 80
+#print( x << 8*(2-1))
 
-
-def encode_alpha(data):
-    return chunkfy(binascii.hexlify(data.encode()).decode())
-
-
-def parse_byte(data, start):
-    return data[start:start+1].decode()
-
-
-def encode_byte(data):
-    return binascii.hexlify(data.encode()).decode()
-
-
-def parse_bitfield(data, start):
-    result = []
-    for i in range(7, -1, -1):
-        if data[start] & (1 << i) == 0:
-            result.append('0')
-        else:
-            result.append('1')
-    return result
-
-
-def encode_bitfield(data):
-    result = 0
-    for i in data:
-        result <<= 1
-        result |= (i & 1)
-    return encode_integer(result, 1)
-
-print(parse_integer(bytes.fromhex("00 10"),0,2))
-print(parse_integer(bytes.fromhex("00 10 00 00"),0,4))
-
-print(parse_signed_integer2(bytes.fromhex("87"),0,1))
-print(parse_signed_integer2(bytes.fromhex("07"),0,1))
-print(parse_signed_integer2(bytes.fromhex("07 80"),0,2))
-print(parse_signed_integer2(bytes.fromhex("07 00"),0,2))
-print(parse_signed_integer2(bytes.fromhex("07 00 00 80"),0,4))
-print(parse_signed_integer2(bytes.fromhex("07 00 00 00"),0,4))
-print(parse_signed_integer2(bytes.fromhex("6c 08 00 80"),0,4))
-print(parse_signed_integer2(bytes.fromhex("6c 08 00 00"),0,4))
-
-print(parse_bitfield(bytes.fromhex("e5"),0))
-
-print(parse_byte(bytes.fromhex("53"),0))
-print(parse_byte(bytes.fromhex("73"),0))
-
-print(parse_date(bytes.fromhex("32 30 31 36 30 34 32 36"),0))
-print(parse_date(bytes.fromhex("31 39 39 39 31 32 33 31"),0))
-print(parse_time(bytes.fromhex("31 33 3a 32 38 3a 30 37"),0))
-
-print(parse_alpha(bytes.fromhex("61 62 63 64 65 66 67 24 31 33 3f"),0,11))
-
-print(encode_integer(4096,2))
-print(encode_signed_integer2(-7,1))
-print(encode_signed_integer2(7,1))
-print(encode_signed_integer2(-7,2))
-print(encode_signed_integer2(7,2))
-print(encode_signed_integer2(-7,4))
-print(encode_signed_integer2(7,4))
-print(encode_signed_integer2(-2156,4))
-print(encode_signed_integer2(2156,4))
-
-print(encode_byte("S"))
-print(encode_byte("s"))
-
-print(encode_alpha("abcdefg$13?"))
-
-print(encode_date("20160426"))
-print(encode_date("19991231"))
-print(encode_time("13:28:07"))
-
-print(parse_signed_integer2(bytes.fromhex("00 e1 f5 05 00 00 00 80"),0,8))
-print(encode_signed_integer2(-100000000,8))
-print(parse_signed_integer2(bytes.fromhex("00 e1 f5 05 00 00 00 00"),0,8))
-print(encode_signed_integer2(100000000,8))
-
-print(parse_signed_integer2(bytes.fromhex("d0 50"),0,2))
-print(parse_signed_integer2(bytes.fromhex("d0 d0"),0,2))
-print(encode_signed_integer2(288,2))
-
-print(parse_signed_integer2(bytes.fromhex("d0"),0,1)) #ok
-print(parse_signed_integer2(bytes.fromhex("50"),0,1)) #ok
-
-print(parse_signed_integer2(bytes.fromhex("00 d0"),0,2))
-print(parse_signed_integer2(bytes.fromhex("00 50"),0,2)) # -> 20480
-
-print(encode_signed_integer2(-20480,2))
-print(encode_signed_integer2(20480,2))
-
-x = 80
-print( x << 8*(2-1))
-
-print("bits")
-print(encode_bitfield([1, 1, 1, 0, 0, 1, 0, 1]))
+#print(encode_bitfield([1, 1, 1, 0, 0, 1, 0, 1]))
--- src/python/texit/mitchcodec.py
+++ src/python/texit/mitchcodec.py
@@ -63,8 +63,10 @@ def encode_integer(data, length):
 
 
 def decode_alpha(data, start, length):
-    return data[start:start+length].decode()
-
+    if length != 0:
+        return data[start:start+length].decode()
+    else:
+        return data[start:].decode()
 
 def encode_alpha(data, length):
     payload = binascii.hexlify(data.encode()).decode()
@@ -90,6 +92,12 @@ def decode_annotated_hex(description):
 
 def encode_annotated_hex(data, length):
     return data[2:data.index("/")]
+
+
+def encode_const_integer(val):
+    def encode_const_integer_internal(data, length):
+        return encode_integer(val,1)
+    return encode_const_integer_internal
 
 
 def decode_hex(data, start, length):
@@ -153,7 +161,7 @@ def encode_price(implied_precision):
     def encode_price_inner(data, length):
         dot_index = data.find('.')
         if dot_index < 0:
-            data += ("0"*implied_precision)
+            data += ("0" * implied_precision)
         else:
             exiting = len(data) - dot_index - 1
             for i in range(0, implied_precision - exiting):
@@ -176,30 +184,35 @@ def parse_msg(data):
         fields[pair[0]] = pair[1]
     return fields
 
+
 def encode_msg_from_tag_vals(handlers, type_decoder, tag_values):
     type_byte = type_decoder(tag_values)
     if type_byte in handlers:
         handler = handlers[type_byte]
         result = " ".join(
             [field_handler[3](tag_values.get(field_handler[0]),field_handler[1]) for field_handler in handler])
-        length = int((len(result) / 3) + 2)  # TODO move outside to IR as a tag length ?
-        return (hex(length)[2:].zfill(2) + " " + result).upper()
+        return result.upper()
     else:
         return None
 
-def decode_msg_from_hex_strig(handlers, type_decoder, value):
+
+def encode_all(handlers, type_decoder, tag_values):
+    return encode_msg_from_tag_vals(handlers, type_decoder, tag_values)
+
+
+def decode_msg_from_hex_string(handlers, type_decoder, value):
     type_byte = type_decoder(value)
-    binary_value = bytes.fromhex(value)
     if type_byte in handlers:
         handler = handlers[type_byte]
+        binary_value = bytes.fromhex(value)
         result = ""
-        offset = 1  # TODO process length as normal tag (add to IR)?
+        offset = 0
         for field_handler in handler:
             val = field_handler[2](binary_value, offset, field_handler[1])
             if val is not None:
                 result += field_handler[0] + "=" + str(val).strip() + " "
             offset += field_handler[1]
-        return result
+        return result[:-1]
     else:
         return None
 
@@ -208,7 +221,7 @@ def decode_msg_from_hex_strig(handlers, type_decoder, value):
 # http://www.londonstockexchange.com/products-and-services/millennium-exchange/technicalinformation/technicalinformation.htm
 mitch_handlers_1 = {
     hex(int("0x41", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(34)),
         ("type", 1, decode_annotated_hex("AddOrder"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -220,7 +233,7 @@ mitch_handlers_1 = {
         ("Flags", 1, decode_annotated_bitfield({(1, "bit1")}), encode_annotated_bitfield)
     ),
     hex(int("0x42", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(19)),
         ("type", 1, decode_annotated_hex("TradeBreak"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("TradeID", 8, decode_integer, encode_integer),
@@ -228,7 +241,7 @@ mitch_handlers_1 = {
         ("InstrumentID", 4, decode_integer, encode_integer),
     ),
     hex(int("0x43", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(39)),
         ("type", 1, decode_annotated_hex("OrderExecutedWithPrice"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -239,7 +252,7 @@ mitch_handlers_1 = {
         ("Price", 8, decode_price(8), encode_price(8)),  # 8 decimal points
     ),
     hex(int("0x44", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(19)),
         ("type", 1, decode_annotated_hex("OrderDeleted"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -247,7 +260,7 @@ mitch_handlers_1 = {
         ("InstrumentID", 4, decode_integer, encode_integer),
     ),
     hex(int("0x45", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(26)),
         ("type", 1, decode_annotated_hex("OrderExecuted"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -255,7 +268,7 @@ mitch_handlers_1 = {
         ("TradeID", 8, decode_integer, encode_integer)
     ),
     hex(int("0x46", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(45)),
         ("type", 1, decode_annotated_hex("AddAttributedOrder"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -268,7 +281,7 @@ mitch_handlers_1 = {
         ("Flags", 1, decode_annotated_bitfield({(2, "bit2"), (3, "bit3")}), encode_annotated_bitfield)
     ),
     hex(int("0x47", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(32)),
         ("type", 1, decode_annotated_hex("SymbolStatus"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("InstrumentID", 8, decode_integer, encode_integer),
@@ -281,12 +294,12 @@ mitch_handlers_1 = {
         ("BookType", 1, decode_integer, encode_integer),
     ),
     hex(int("0x54", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(6)),
         ("type", 1, decode_annotated_hex("Time"), encode_annotated_hex),
         ("Seconds", 4, decode_integer, encode_integer),
     ),
     hex(int("0x55", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(27)),
         ("type", 1, decode_annotated_hex("OrderModified"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -295,19 +308,22 @@ mitch_handlers_1 = {
         ("Flags", 1, decode_annotated_bitfield({(0, "bit0"), (5, "bit5")}), encode_annotated_bitfield)
     ),
     hex(int("0x79", 16)): (
-        #"length": "length",
+        ("length", 1, decode_blank, encode_const_integer(13)),
         ("type", 1, decode_annotated_hex("OrderBookClear"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("InstrumentID", 4, decode_integer, encode_integer),
         ("Rsv", 2, decode_blank, encode_blank),
         ("Flags", 1, decode_annotated_bitfield({(5, "bit5")}), encode_annotated_bitfield),
+    ),
+    hex(int("0x3B", 16)): (
+        ("auxiliary", 0, decode_quoted_alpha, encode_quoted_alpha),
     )
 }
 
 #http://www.oslobors.no/ob_eng/Oslo-Boers/Trading/Trading-systems/Millennium-Exchange/Technical-documentation
 mitch_handlers_2 = {
     hex(int("0x41", 16)): (
-        # ("length", "byte"),
+        ("length", 1, decode_blank, encode_const_integer(46)),
         ("type", 1, decode_annotated_hex("AddOrder"), encode_annotated_hex),
         ("Nanosecond", 4, decode_integer, encode_integer),
         ("OrderID", 8, decode_integer, encode_integer),
@@ -325,34 +341,25 @@ mitch_handlers_2 = {
 mitch_handlers_3 = {
 
 }
-type_from_tag_vals = lambda tag_values: hex(int(tag_values.get("type")[:tag_values.get("type").index("/")], 16))
+type_from_tag_vals = lambda tag_values: hex(int(tag_values.get("type")[:tag_values.get("type").index("/")], 16)) \
+    if "type" in tag_values else hex(int("0x3B", 16))
 type_from_hex_string = lambda value: hex(int("0x" + value[3:5],16))
 
 ######################################
 
-def test_from_text_msg(schema, type_from_tag_vals, text_message):
+def test_from_text_msg(schema, type_from_tag_vals, type_from_hex_string, text_message):
     msg_tag_values = parse_msg(text_message)
-    hex_message = encode_msg_from_tag_vals(schema, type_from_tag_vals, msg_tag_values)
+    hex_message = encode_all(schema, type_from_tag_vals, msg_tag_values)
     print(text_message)
     print(hex_message)
-    print(decode_msg_from_hex_strig(schema, type_from_hex_string, hex_message))
+    print(decode_msg_from_hex_string(schema, type_from_hex_string, hex_message))
     print("")
 
+def test_from_hex_msg(schema, type_from_tag_vals, type_from_hex_string, hex_message):
+    text_message = decode_msg_from_hex_string(schema, type_from_hex_string, hex_message)
+    print(hex_message)
+    print(text_message)
+    msg_tag_values = parse_msg(text_message)
+    print(encode_all(schema, type_from_tag_vals, msg_tag_values))
+    print("")
 
-#test_from_text_msg(mitch_handlers_1, "type=0x41/AddOrder Nanosecond=007706000 OrderID=363367994316859631 Side='S' Quantity=2500 InstrumentID=37908 Price=42.95000000 Flags=0x00/bit1=0")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x46/AddAttributedOrder Nanosecond=683608000 OrderID=363526361572180373 Side='B' Quantity=200000 InstrumentID=5090 Price=33.00000000 Attribution='WNTSGB2LBIC' Flags=0x20/bit2=0,bit3=1")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x44/OrderDeleted Nanosecond=088393000 OrderID=363526361572180374 Flags=0x00/bit4=0 InstrumentID=5081")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x55/OrderModified Nanosecond=306445000 OrderID=363526361572180587 NewQuantity=200000 NewPrice=36.00000000 Flags=0x08/bit0=0,bit5=1")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x79/OrderBookClear Nanosecond=34344000 InstrumentID=21472 Flags=0x08/bit5=1")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x54/Time Seconds=18010")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x45/OrderExecuted Nanosecond=211867000 OrderID=363526341171088975 ExecutedQuantity=31 TradeID=1420024233328668")
-
-#test_from_text_msg(mitch_handlers_1, "type=0x43/OrderExecutedWithPrice Nanosecond=177651000 OrderID=363526341171092635 ExecutedQuantity=7 DisplayQuantity=2 TradeID=1420024233328641 Printable='N' Price=755.00000000")
-
-#test_from_text_msg(mitch_handlers_2, "type=0x41/AddOrder Nanosecond=1 OrderID=2 Side='B' Quantity=34 InstrumentID=4 Price=5.00000000 Flags=0x01/bit1=0")
